e62113082bd2dcf268bf6fe77e225ac4ad840afd
Gil96 <36649338+Gil96@users.noreply.github.com>
2020-03-30 01:01:51 +0100

    Update proc_line_atk.c
    jsut forgot

--- Processes_cache_line_L1/proc_line_atk.c
+++ Processes_cache_line_L1/proc_line_atk.c
@@ -1,10 +1,18 @@
 /* 
-proc_line_atk.c
-
-DESCRIPTION:
-        this is just a try to 
-        1)  run a victim process on the background
-        2)  launch an attacker process in other terminal in order to discorer the line in the cache that victims is making use of.
+    
+    >>  make
+    >>  ./vic
+    >>  ./atk
+    >> cat results.out
+    CPU details:
+        model name.............. Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz
+        CPU(s).................. 8
+        On-line CPU(s) list..... 0-7
+        Thread(s) per core...... 2
+        Core(s) per socket...... 4
+        L1d_cache_size.......... 32768 B
+        L1d_assoc............... 8
+        L1d_line_size........... 64 B    
 */
 
 #include <pthread.h>
